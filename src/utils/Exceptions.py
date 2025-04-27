f9bff68767cfc1a504ae726ee2cf392bae60fad3
Matvey <yslmego248@gmail.com>
2025-04-27 01:22:13 -0700

    custom exceptions

---
+++ src/utils/Exceptions.py
@@ -0,0 +1,8 @@
+class ValidationError(Exception):
+    """Исключение для обработки ошибок валидации."""
+    pass
+
+
+class SharedStringError(Exception):
+    """Исключение для обработки ошибок с excel файлом SharedStrings.xml."""
+    pass
